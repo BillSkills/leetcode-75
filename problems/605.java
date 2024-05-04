8a2768a542cb2893c91eef17394144ca6da254c1
Bill_Skills <billskills01@gmail.com>
2024-05-04 14:14:54 -0400

    May 4

---
+++ problems/605.java
@@ -0,0 +1,24 @@
+/*
+[AC 05/04/2024]
+605. Can Place Flowers
+https://leetcode.com/problems/can-place-flowers/
+*/
+
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int placed = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                boolean left = (i == 0 || flowerbed[i - 1] == 0);
+                boolean right = (i == flowerbed.length - 1 || flowerbed[i + 1] == 0);
+                if (left && right) {
+                    flowerbed[i] = 1;
+                    placed++;
+                    if (placed >= n)
+                        return true;
+                }
+            }
+        }
+        return placed >= n;
+    }
+}
