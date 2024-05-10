60588a65c118acf89fc9d029f02d4f03e80a4c10
Bill_Skills <billskills01@gmail.com>
2024-05-10 11:17:55 -0400

    May 10

---
+++ problems/python/283.py
@@ -0,0 +1,14 @@
+# [AC 05/10/2024]
+# 283. Move Zeroes
+# https://leetcode.com/problems/move-zeroes/
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        left = 0
+
+        # move all nonzeroes to left
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                # swap values at left and right ptrs.
+                nums[right], nums[left] = nums[left], nums[right]
+                left += 1 # increment left ptr, like 'wall' in quicksort partition
