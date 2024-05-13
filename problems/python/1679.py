ad555b3bfa69efca509fcf1c7cedc56c1ab94426
Bill_Skills <billskills01@gmail.com>
2024-05-13 10:59:36 -0400

    May 13

---
+++ problems/python/1679.py
@@ -0,0 +1,23 @@
+# [AC 05/13/2024]
+# 1679. Max Number of K-Sum Pairs
+# https://leetcode.com/problems/max-number-of-k-sum-pairs/
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+
+        i = 0
+        j = len(nums) - 1
+        ops = 0
+
+        while i < j:
+            if nums[i] + nums[j] == k:
+                i += 1
+                j -= 1
+                ops += 1
+            elif nums[i] + nums[j] < k:
+                i += 1
+            else:
+                j -= 1
+        
+        return ops
