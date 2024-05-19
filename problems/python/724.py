09170e0bba74e6bb9eab09044724c8ddf8b57a92
Bill_Skills <billskills01@gmail.com>
2024-05-19 11:54:24 -0400

    May 19

---
+++ problems/python/724.py
@@ -0,0 +1,16 @@
+# [AC 05/19/2024]
+# 724. Find Pivot Index
+# Prefix Sum
+# https://leetcode.com/problems/find-pivot-index/
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left, right = 0, sum(nums)
+
+        for i in range(len(nums)):
+            right -= nums[i]
+            if left == right:
+                return i
+            left += nums[i]
+        
+        return -1
