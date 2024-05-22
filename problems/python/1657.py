6feb6735625fd53c7fe8d0a0728d94248755b172
Bill_Skills <billskills01@gmail.com>
2024-05-22 11:40:40 -0400

    May 22

---
+++ problems/python/1657.py
@@ -0,0 +1,30 @@
+# [AC 05/22/2024]
+# 1657. Determine if Two Strings are Close
+# Hash Map / Set
+# https://leetcode.com/problems/determine-if-two-strings-are-close/
+
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2): return False
+
+        freq1 = [0] * 26
+        freq2 = [0] * 26
+
+        for c in word1:
+            freq1[ord(c) - ord('a')] += 1
+        
+        for c in word2:
+            freq2[ord(c) - ord('a')] += 1
+        
+        for i in range(26):
+            if (freq1[i] == 0 and freq2[i] != 0) or (freq1[i] != 0 and freq2[i] == 0):
+                return False
+        
+        freq1.sort()
+        freq2.sort()
+
+        for i in range(26):
+            if freq1[i] != freq2[i]:
+                return False
+        
+        return True
